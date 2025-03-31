05bc8a21210057231c21e2cc7ec3baab0efd25dc
dazz6 <dazz6@kakao.com>
2025-03-31 19:26:20 +0900

    번역 css 를 위한 function 수정

--- src/Translator.tsx
+++ src/Translator.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
-import { colors } from './GlobalStyle';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { colors } from "./GlobalStyle";
 
 const ButtonContainer = styled.div`
   display: flex;
@@ -11,28 +11,43 @@ const ButtonContainer = styled.div`
   padding: 1rem;
 `;
 
-const LangButton = styled.button`
+const LangButton = styled.button<{ now: boolean }>`
   font-size: 0.8rem;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
   border: none;
   background-color: transparent;
-  color: ${colors.brown0};
+  color: ${(props) => (props.now ? colors.brown0 : colors.brown2)};
+  font-weight: ${(props) => (props.now ? `bold` : `none`)};
 
   &:hover {
-    color: ${colors.brown2};
+    color: ${colors.brown0};
   }
 `;
 
 export default function Translator() {
+  const [langState, setLangState] = useState("ko");
   const { i18n } = useTranslation();
+
+  const changeProc = (value: string) => {
+    i18n.changeLanguage(value);
+    setLangState(value);
+  };
 
   return (
     <ButtonContainer>
-      <LangButton onClick={() => i18n.changeLanguage('en')}>English</LangButton>
-      <LangButton onClick={() => i18n.changeLanguage('ko')}>한국어</LangButton>
-      <LangButton onClick={() => i18n.changeLanguage('ja')}>日本語</LangButton>
-      <LangButton onClick={() => i18n.changeLanguage('zh')}>中文</LangButton>
+      <LangButton now={langState === "en"} onClick={() => changeProc("en")}>
+        English
+      </LangButton>
+      <LangButton now={langState === "ko"} onClick={() => changeProc("ko")}>
+        한국어
+      </LangButton>
+      <LangButton now={langState === "ja"} onClick={() => changeProc("ja")}>
+        日本語
+      </LangButton>
+      <LangButton now={langState === "zh"} onClick={() => changeProc("zh")}>
+        中文
+      </LangButton>
     </ButtonContainer>
   );
 }
